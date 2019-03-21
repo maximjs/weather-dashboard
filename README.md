f2b0ead5ba5bf413b7d0f7f8083ac3172ca4fb59
Kozlyakov Maxim <maxim.kozlyakov@gmail.com>
2019-03-21 13:34:20 +0300

    all done

--- README.md
+++ README.md
@@ -1,68 +1,27 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Результат:  
 
-## Available Scripts
+***
 
-In the project directory, you can run:
+### Тестовое задание на позицию frontend разработчика
 
-### `npm start`
+Написать простое приложение - Weather dashboard, используя
+https://openweathermap.org/api​(есть бесплатный тариф).
+Отправная визуальная точка из HTML + CSS показаны тут:
+https://jsfiddle.net/8pqekfaL/26/
 
-Runs the app in the development mode.<br>
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+Управление посредством 3 контролов:
+  * текстовое поле - для ввода названия города, например, Moscow. Разрешается ввод только латинских букв и цифр.
+  * кнопка с надписью "Add" - добавляет погоду на dashboard
+  * кнопка с надписью "Clear" - очищает поле ввода
 
-The page will reload if you make edits.<br>
-You will also see any lint errors in the console.
+Отображаемая погода должна включать в себя: название города, градусы цельсия и иконка погоды.
+Если на дашборде нет ни одного города, то нужно отображать по центру надпись - Dashboard is empty.
+По наведении мышки появляется кнопка Delete. По нажатии на нее необходимо удалять данную погоду с дашборда.
+Если нет информации по введенному городу, то выводить сообщение об ошибке в названии.
 
-### `npm test`
-
-Launches the test runner in the interactive watch mode.<br>
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.<br>
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.<br>
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (Webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
-
-### Analyzing the Bundle Size
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
-
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `npm run build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+#### Требования
+  * Результат в виде репозитория на github
+  * При создании проекта использовать​ create-react-app
+  * Для AJAX запросов использовать Fetch API
+  * Необходимо отображение процесса загрузки данных (loader/spinner etc)
+  * Необходимо сохранять состояния дашборда между обновлениями вкладки
--- src/App.js
+++ src/App.js
@@ -1,25 +1,137 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import cn from 'classnames';
+import ReactLoading from 'react-loading';
+import styles from './App.module.sass';
+
+const apiKey = 'ff143ed1daddb873cb08192d68e3aa1a';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    const initData = JSON.parse(localStorage.getItem('cities'));
+    this.state = {
+        inputValue: '',
+        isValidInput: null,
+        isLoading: false,
+        data: initData || [],
+        error: '',
+      };
+  }
+
+  handleInput = e => {
+    const { value } = e.target;
+    const isValid = this.validate(value);
+    const message = (!isValid && value !== '') ? `Use only Latin letters and numbers` : '';
+    this.setState({
+      inputValue: value,
+      isValidInput: value === '' ? null : isValid,
+      error: message,
+    });
+  }
+
+  validate = string => {
+    return (string.search(/^[a-zA-Z0-9]+$/i) !== -1);
+  }
+
+  handleInputClear = () => {
+    this.setState({ inputValue: '', isValidInput: null, error: '' });
+  }
+
+  handleDelete = id => () => {
+    const { data } = this.state;
+    const filteredData = data.filter(el => el.id !== id);
+    this.saveToStorage(filteredData);
+    this.setState({ data: filteredData });
+  }
+
+  saveToStorage = data => {
+    const preparedData = JSON.stringify(data);
+    localStorage.setItem('cities', preparedData);
+  }
+
+  handleAdd = () => {
+    const { inputValue, isValidInput, data } = this.state;
+    const isCityInData = data.findIndex(item => item.name.toLowerCase() === inputValue.toLowerCase()) !== -1;
+    if (isCityInData) {
+      const message = `The city ${inputValue} is already on the dashboard`;
+      this.setState({ inputValue: '', isValidInput: null, error: message });
+      return;
+    }
+    if (!inputValue || !isValidInput) {
+      return;
+    }
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&units=metric&appid=${apiKey}`;
+    this.setState({ isLoading: true, error: '' });
+    fetch(url)
+      .then(response => response.json())
+      .then(resp => {
+        if (resp.cod !== 200) {
+          throw new Error(resp.message);
+        }
+        const dataResp = {
+          id: resp.id,
+          name: resp.name,
+          temp: resp.main.temp,
+          description: resp.weather[0].description,
+          iconcode: resp.weather[0].icon,
+        };
+        const newData = [...data, dataResp];
+        this.saveToStorage(newData);
+        this.setState({
+          isLoading: false,
+          data: newData,
+          inputValue: '',
+          isValidInput: null,
+        });
+      })
+      .catch(err => {
+        this.setState({ isLoading: false, error: err.message });
+      });
+  }
+
+  renderDashboard = () => {
+    const { data } = this.state;
+    return data.map(item => {
+      const { id, name, temp, description, iconcode } = item;
+      const sign = temp >= 0 ? '+' : '';
+      const temperature = `${sign}${temp.toFixed(1)} ℃`;
+      const iconUrl = 'http://openweathermap.org/img/w/' + iconcode + '.png';
+      return (
+        <div className={styles.weather} key={id}>
+          <h3>{name}</h3>
+          <div>{temperature}</div>
+          <img src={iconUrl} alt={description} />
+          <div onClick={this.handleDelete(id)} className={styles.weatherDelete}>Delete</div>
+        </div>
+      );
+    });
+  };
+
+  renderEmpty = () => (
+    <div className={styles.dashboardPlaceholder}>
+      Dashboard is empty
+    </div>
+  );
+
   render() {
+    const { inputValue, isValidInput, data, isLoading, error } = this.state;
+    const inputClass = cn({
+      [styles.input]: true,
+      [styles.inputValid]: isValidInput === true,
+      [styles.inputInvalid]: isValidInput === false,
+    });
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className={styles.app}>
+        <div className={styles.inputCity}>
+          <input className={inputClass} onChange={this.handleInput} value={inputValue} placeholder="City..." />
+          <button className={styles.inputCityAdd} onClick={this.handleAdd} disabled={isLoading}>Add</button>
+          <button className={styles.inputCityDelete} onClick={this.handleInputClear} disabled={isLoading}>Clear</button>
+          {error && <div className={styles.error}>{error}</div>}
+          {isLoading && <ReactLoading type="spinningBubbles" color="grey" width="40" height="40" className={styles.loading} />}
+        </div>
+        <div className={styles.dashboard}>
+          {data.length > 0 ? this.renderDashboard() : this.renderEmpty()}
+        </div>
       </div>
     );
   }
