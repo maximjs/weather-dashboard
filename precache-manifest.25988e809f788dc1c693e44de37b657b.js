f0b384c923fcb89a01cff714acd12cf65e49ab8f
Kozlyakov Maxim <maxim.kozlyakov@gmail.com>
2019-03-21 13:29:58 +0300

    Updates

---
+++ precache-manifest.25988e809f788dc1c693e44de37b657b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9626dc00f8c113de48f",
+    "url": "/weather-dashboard/static/css/main.974c4757.chunk.css"
+  },
+  {
+    "revision": "d9626dc00f8c113de48f",
+    "url": "/weather-dashboard/static/js/main.ae8fde20.chunk.js"
+  },
+  {
+    "revision": "18eb3f1a32e80f41731d",
+    "url": "/weather-dashboard/static/js/runtime~main.4e9c5ee0.js"
+  },
+  {
+    "revision": "a9b3052055d843a7f1d0",
+    "url": "/weather-dashboard/static/js/2.f7d6fe98.chunk.js"
+  },
+  {
+    "revision": "ee0e741a3b92dfde7870314edc4896b2",
+    "url": "/weather-dashboard/index.html"
+  }
+];
